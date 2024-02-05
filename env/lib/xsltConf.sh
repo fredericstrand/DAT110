dbb7accf69d35c27fd343be698cf0077f960d182
fredericstrand <strandfrederic@outlook.com>
2024-02-05 10:42:31 +0100

    firs commit

---
+++ env/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/fredericstrand/Documents/DAT110/env/lib"
+XSLT_LIBS="-lxslt -L/Users/fredericstrand/Documents/DAT110/env/lib -lxml2 "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/fredericstrand/Documents/DAT110/env/include"
+MODULE_VERSION="xslt-1.1.37"
